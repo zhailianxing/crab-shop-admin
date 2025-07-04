93cbc903b98f21f69b7f2b87a73d903e8f08e647
unknown <zhailianxing@ztgame.com>
2025-07-04 10:55:54 +0800

    feat: 05 首页图表也使用el-row布局、图表上选项卡使用el-check-tag

--- src/page/index.vue
+++ src/page/index.vue
@@ -7,7 +7,7 @@
                     <el-skeleton animated>
                         <template #template>
                             <!-- 将真正dom的el-card拷贝到这里，然后用el-skeleton-item代替 实际dom -->
-                            <el-card>
+                            <el-card class="card">
                                 <template #header>
                                     <div class="card-header">
                                         <el-skeleton-item variant="text" style="margin-right: 16px" />
@@ -27,7 +27,7 @@
                 </el-col>
 
                 <el-col v-for="(item, index) in panels" :key="index" :span="6">
-                    <el-card>
+                    <el-card class="card">
                         <template #header>
                             <div class="card-header">
                                 <span style="font-size: 16px;">{{ item.title }}</span>
@@ -46,11 +46,32 @@
             </el-row>
 
 
-            <!-- 第一块： 功能图标 -->
+            <!-- 第二块： 功能图标 -->
+            <icon-tab></icon-tab>
+
+            <!-- 第三块:  echart图表展示统计信息 -->
+            <el-row>
+                <el-col :span="12" :offset="0">
+                    <el-card class="echart-layout">
+                        <template #header>
+                            <div class="echart-layout-header">
+                                <span>订单统计</span>
+                                <div class="flex gap-2">
+                                    <el-check-tag :checked="currentTab == item.value" v-for="(item, index) in echarTabs"
+                                        @click="handleClickTab(item.value)">
+                                        {{ item.label }}
+                                    </el-check-tag>
+                                </div>
+                            </div>
+                        </template>
+                        <!-- card body -->
+                    </el-card>
+                </el-col>
+                <el-col :span="12" :offset="0"></el-col>
+            </el-row>
 
         </div>
 
-        <icon-tab></icon-tab>
 
 
     </div>
@@ -71,22 +92,57 @@ onBeforeMount(async () => {
         panels.value = res.data.panels
     }
 })
+
+
+
+// echar 图表
+
+// 选项卡
+const currentTab = ref("week")
+const echarTabs = [
+    {
+        label: "近1个月",
+        value: "month"
+    }, {
+        label: "近1周",
+        value: "week"
+    }, {
+        label: "近24小时",
+        value: "day"
+    },
+]
+// 选项卡切换 
+const handleClickTab = (selectedValue) => {
+    currentTab.value = selectedValue
+}
+
 </script>
 
 
 <style lang="scss" scoped>
-.el-card {
+.card {
     max-width: 480px;
     margin: 0 10px;
+
+    .card-header {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .card-footer {
+        display: flex;
+        justify-content: space-between;
+    }
 }
 
-.card-header {
-    display: flex;
-    justify-content: space-between;
-}
 
-.card-footer {
-    display: flex;
-    justify-content: space-between;
+.echart-layout {
+    width: 100%;
+    margin-top: 10px;
+
+    .echart-layout-header {
+        display: flex;
+        justify-content: space-between;
+    }
 }
 </style>
